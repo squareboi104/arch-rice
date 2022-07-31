ebe0db00b790536e963ede75bba574631c5267b1
Manu <manuel.aller.07@gmail.com>
2022-08-01 00:15:29 +0200

    coc.nvim commands

--- arch-rice.sh
+++ arch-rice.sh
@@ -4,7 +4,7 @@
 # License: MIT
 
 # Variables
-PACKAGES="base-devel xorg xorg-xinit doas vim xorg-drivers dunst neovim zsh stow exa zip unzip unclutter pcmanfm htop firefox gimp mpv zsh-syntax-highlighting sl virtualbox virtualbox-host-modules-arch pulseaudio pulsemixer lutris feh steam rxvt-unicode git zathura flameshot alsa-utils imagemagick openssh"
+PACKAGES="base-devel xorg xorg-xinit doas vim xorg-drivers dunst neovim zsh stow exa zip unzip unclutter pcmanfm htop firefox gimp mpv zsh-syntax-highlighting sl virtualbox virtualbox-host-modules-arch pulseaudio pulsemixer lutris feh steam rxvt-unicode git zathura flameshot alsa-utils imagemagick openssh nodejs"
 #AUR_PACKAGES="pfetch"
 GIT_DWM="https://github.com/squareboi104/dwm.git"
 GIT_SLSTATUS="https://github.com/squareboi104/slstatus.git"
@@ -42,6 +42,11 @@ sudo cp $HOME/arch-rice/doas.conf /etc/ &&
 # Change shell
 echo "Changing shell"
 chsh -s /bin/zsh #>/dev/null 2>&1
+
+# Node.js coc.nvim
+echo "Enter root password"
+su -c "curl -sL install-node.now.sh | bash"
+nvim -c "CocInstall -sync coc-jedi coc-sh"
 
 # Virtualbox
 sudo modprobe vboxdrv
